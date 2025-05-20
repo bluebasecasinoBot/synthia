af5058f32d1c5f962178fcedd9c0b213c106582e
bluebasecasinoBot <developerme33@gmail.com>
2025-05-20 22:12:47 +0100

    Update footer.js

--- footer.js
+++ footer.js
@@ -61,7 +61,7 @@ __SYD.footer_main_child2 = () =>{
                                 {style:'display:flex;column-gap:10px;padding:5px;width:fit-content;align-items:center'},
                                 [
                                     __SYD.iconify_20px({attr:'fa-brands fa-x-twitter' , clr :'#015484'}),//<i class="fa-solid fa-envelope-open-text"></i>
-                                    __c('a',{href:"ivviera@gmail.com"},[__SYD.text_lib_type_p_glob_vmax({cnt:'Twitter', size:'14px' , w:'500',clr:'#015484'})]),
+                                    __c('a',{href:"https://x.com/synthiasyai"},[__SYD.text_lib_type_p_glob_vmax({cnt:'Twitter', size:'14px' , w:'500',clr:'#015484'})]),
                                 ]
                             ),
                 
@@ -70,7 +70,7 @@ __SYD.footer_main_child2 = () =>{
                                 {style:'display:flex;column-gap:10px;padding:5px;width:fit-content;align-items:center'},
                                 [
                                     __SYD.iconify_20px({attr:'fa-brands fa-telegram' , clr :'#015484'}),//<i class="fa-solid fa-phone-volume"></i>
-                                    __c('a',{href:"tel:07072111027"},[__SYD.text_lib_type_p_glob_vmax({cnt:'Telegram', size:'14px' , w:'500',clr:'#015484'})]),
+                                    __c('a',{href:"https://t.me/synthAI_eth"},[__SYD.text_lib_type_p_glob_vmax({cnt:'Telegram', size:'14px' , w:'500',clr:'#015484'})]),
                                 ]
                             ),
                 
